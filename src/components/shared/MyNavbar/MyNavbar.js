83cd2af028a3cc4154190bb5f7d4c4257122fdf1
Randy Tate <randytatejr@gmail.com>
2020-01-08 20:50:16 -0600

    completed navbar

--- src/components/shared/MyNavbar/MyNavbar.js
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -1,12 +1,59 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import { Link } from 'react-router-dom';
+
+import PropTypes from 'prop-types';
 
 import './MyNavbar.scss';
 
 class MyNavbar extends React.Component {
+  static propTypes = {
+    authed: PropTypes.bool,
+  };
+
+  logMeOut = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  }
+
   render() {
+    const { authed } = this.props;
+
+    const buildNavbar = () => {
+      if (authed) {
+        return (
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/home">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/stuff">My Stuff</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/stuff/new">New</Link>
+            </li>
+            <li className="nav-item">
+              <button className="nav-link btn btn-danger" onClick={this.logMeOut}>Log Out</button>
+            </li>
+          </ul>
+        );
+      }
+      return (<ul className="navbar-nav ml-auto"></ul>);
+    };
+
     return (
       <div className="MyNavbar">
-        <h1>My Navbar</h1>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/home">Hoarder</Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            { buildNavbar() }
+          </div>
+        </nav>
       </div>
     );
   }
