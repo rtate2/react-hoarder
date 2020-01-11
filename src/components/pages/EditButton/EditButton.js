3a3d1f980115b13ea8b284c820d6714a45133ce9
Randy Tate <randytatejr@gmail.com>
2020-01-11 09:33:26 -0600

    completed MyStuff buttons

---
+++ src/components/pages/EditButton/EditButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './EditButton.scss';
+
+class EditButton extends React.Component {
+  render() {
+    return (
+      <div className="EditButton">
+        <h1>Edit</h1>
+      </div>
+    );
+  }
+}
+
+export default EditButton;
---
+++ src/components/pages/SingleButton/SingleButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './SingleButton.scss';
+
+class SingleButton extends React.Component {
+  render() {
+    return (
+      <div className="SingleButton">
+        <h1>Single Stuff</h1>
+      </div>
+    );
+  }
+}
+
+export default SingleButton;
